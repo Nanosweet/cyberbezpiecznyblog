dbae3b99d7158be18eb7254e1e4b0608533dbf73
Nanosweet <nanosweet321@gmail.com>
2019-10-19 07:56:42 +0200

    dodanie sekcji komenarzy w article_annonymous.html.twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\User;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -15,23 +16,35 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/{slug}", name="app_article")
      */
-    public function showArticle(UserInterface $user, $slug, EntityManagerInterface $entityManager)
+    public function showArticle($slug, EntityManagerInterface $entityManager)
     {
-        $repository = $entityManager -> getRepository(Article::class);
+        $repository = $entityManager->getRepository(Article::class);
         /** @var Article $article */
 
-        $article = $repository -> findOneBy(['slug' => $slug]);
+        $article = $repository->findOneBy(['slug' => $slug]);
 
         /* wyciagniecie id zalogowanego usera */
-        $user_id = $user->getId();
+        if ($this->isGranted("IS_AUTHENTICATED_FULLY")) {
+            /** @var User $user */
+            $user = $this->getUser();
+            $user_id = $user->getId();
 
-        if (!$article) {
-            throw $this->createNotFoundException(sprintf('Brak artykułu: "%s"', $slug));
+            if (!$article) {
+                throw $this->createNotFoundException(sprintf('Brak artykułu: "%s"', $slug));
+            }
+
+            return $this->render('article/article.html.twig', [
+                'controller_name' => 'ArticleController',
+                'article' => $article,
+                'user_id' => $user_id,
+            ]);
         }
-        return $this->render('article/article.html.twig', [
+
+        return $this->render('article/article_annonymous.html.twig', [
             'controller_name' => 'ArticleController',
             'article' => $article,
-            'user_id' => $user_id,
         ]);
     }
+
+
 }
